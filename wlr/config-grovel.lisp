c3e2ae6318c8abbe0767f0031ae3ebc6837f330a
sdilts <stuart.dilts@gmail.com>
2018-10-08 21:38:56 -0600

    Remove features for now

--- wlr/config-grovel.lisp
+++ wlr/config-grovel.lisp
@@ -2,17 +2,17 @@
 (pkg-config-cflags "wlroots")
 (include "wlr/config.h")
 
-(feature :wlr-libcap "WLR_HAS_LIBCAP")
-(feature :wlr-systemd "WLR_HAS_SYSTEMD")
-(feature :wlr-elogind "WLR_HAS_ELOGIND")
+;; (feature :wlr-libcap "WLR_HAS_LIBCAP")
+;; (feature :wlr-systemd "WLR_HAS_SYSTEMD")
+;; (feature :wlr-elogind "WLR_HAS_ELOGIND")
 
-(feature :wlr-x11-backend "WLR_HAS_X11_BACKEND")
+;; (feature :wlr-x11-backend "WLR_HAS_X11_BACKEND")
 
-(feature :wlr-xwayland "WLR_HAS_XWAYLAND")
+;; (feature :wlr-xwayland "WLR_HAS_XWAYLAND")
 
-(feature :wlr-xcb-errors "WLR_HAS_XCB_ERRORS")
-(feature :wlr-xcb-icccm "WLR_HAS_XCB_ICCCM")
-(feature :wlr-xbc-xkb "WLR_HAS_XCB_XKB")
+;; (feature :wlr-xcb-errors "WLR_HAS_XCB_ERRORS")
+;; (feature :wlr-xcb-icccm "WLR_HAS_XCB_ICCCM")
+;; (feature :wlr-xbc-xkb "WLR_HAS_XCB_XKB")
 
-;; don't know if this one is really needed:
-(feature :wlr-posix-fallocate "WLR_HAS_POSIX_FALLOCATE")
+;; ;; don't know if this one is really needed:
+;; (feature :wlr-posix-fallocate "WLR_HAS_POSIX_FALLOCATE")
